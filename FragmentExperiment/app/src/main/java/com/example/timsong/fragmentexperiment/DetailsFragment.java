431483636d7cf437ac82f8ba16386d23b565b0f1
Tim Song <timmy_song@163.com>
2017-01-05 14:44:20 +0800

    Verify the Fragment’s lifecycle callbacks, without testing the restore from back stack case.

--- FragmentExperiment/app/src/main/java/com/example/timsong/fragmentexperiment/DetailsFragment.java
+++ FragmentExperiment/app/src/main/java/com/example/timsong/fragmentexperiment/DetailsFragment.java
@@ -1,8 +1,10 @@
 package com.example.timsong.fragmentexperiment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +18,7 @@ import android.widget.TextView;
 
 public class DetailsFragment extends Fragment {
     private static final String PLAY_INDEX = "selected_play_index";
+    private static final String TAG = "DetailsFragment";
 
     /**
      * Create a new instance of DetailsFragment, initialized to show the text for
@@ -40,6 +43,7 @@ public class DetailsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView is called.");
         ScrollView scrollView = new ScrollView(getActivity());
         TextView textView = new TextView(getActivity());
         int padding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16, getActivity().getResources().getDisplayMetrics());
@@ -49,5 +53,77 @@ public class DetailsFragment extends Fragment {
         textView.setText(Shakespeare.DIALOGUE[getShownIndex()]);
 
         return scrollView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d(TAG, "onAttach is called.");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate is called.");
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, "onActivityCreated is called.");
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        Log.d(TAG, "onViewStateRestored is called.");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart is called.");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume is called.");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause is called.");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState is called.");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop is called.");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView is called.");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy is called.");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "onDetach is called.");
     }
 }
