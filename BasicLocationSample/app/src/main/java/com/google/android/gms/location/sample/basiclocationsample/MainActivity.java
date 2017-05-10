4248804656bbb956139f58a5ac81e237b855410c
Tim Song <timmy_song@163.com>
2017-05-10 17:30:55 +0800

    Add GooglePlayService and Location Permission check functions.

--- BasicLocationSample/app/src/main/java/com/google/android/gms/location/sample/basiclocationsample/MainActivity.java
+++ BasicLocationSample/app/src/main/java/com/google/android/gms/location/sample/basiclocationsample/MainActivity.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2014 Google Inc. All Rights Reserved.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,14 +16,21 @@
 
 package com.google.android.gms.location.sample.basiclocationsample;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
@@ -31,7 +38,7 @@ import com.google.android.gms.location.LocationServices;
 
 /**
  * Location sample.
- *
+ * <p>
  * Demonstrates use of the Location API to retrieve the last known location for a device.
  * This sample uses Google Play services (GoogleApiClient) but does not need to authenticate a user.
  * See https://github.com/googlesamples/android-google-accounts/tree/master/QuickStart if you are
@@ -41,6 +48,8 @@ public class MainActivity extends AppCompatActivity implements
         ConnectionCallbacks, OnConnectionFailedListener {
 
     protected static final String TAG = "MainActivity";
+    private static final int REQUEST_FIX_FAILURE = 1;
+    private static final int REQUEST_COARSE_LOCATION = 2;
 
     /**
      * Provides the entry point to Google Play services.
@@ -56,8 +65,6 @@ public class MainActivity extends AppCompatActivity implements
     protected String mLongitudeLabel;
     protected TextView mLatitudeText;
     protected TextView mLongitudeText;
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -97,6 +104,20 @@ public class MainActivity extends AppCompatActivity implements
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_COARSE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    // Get location info.
+                    getLastKnownLocation();
+                else
+                    Toast.makeText(this, "Location permission is denied.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
     /**
      * Runs when a GoogleApiClient object successfully connects.
      */
@@ -106,6 +127,34 @@ public class MainActivity extends AppCompatActivity implements
         // applications that do not require a fine-grained location and that do not need location
         // updates. Gets the best and most recent location currently available, which may be null
         // in rare cases when a location is not available.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                // Need to explain the reason/rationale of the permission.
+                DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case DialogInterface.BUTTON_POSITIVE:
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_COARSE_LOCATION);
+                                break;
+                        }
+                    }
+                };
+                new AlertDialog.Builder(this).setMessage("Permission needed to get location of your current device:)")
+                        .setPositiveButton("Understand", dialogListener)
+                        .setNegativeButton("Not need it", dialogListener)
+                        .create().show();
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_COARSE_LOCATION);
+            }
+        } else {
+            getLastKnownLocation();
+        }
+    }
+
+    private void getLastKnownLocation() {
+        // Permission is checked outside of the method.
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         if (mLastLocation != null) {
             mLatitudeText.setText(String.format("%s: %f", mLatitudeLabel,
@@ -114,6 +163,7 @@ public class MainActivity extends AppCompatActivity implements
                     mLastLocation.getLongitude()));
         } else {
             Toast.makeText(this, R.string.no_location_detected, Toast.LENGTH_LONG).show();
+            // The Emulator has no last known location to report:)
         }
     }
 
@@ -122,6 +172,14 @@ public class MainActivity extends AppCompatActivity implements
         // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
         // onConnectionFailed.
         Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+        GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this, REQUEST_FIX_FAILURE, new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                if (BuildConfig.DEBUG) {
+                    Log.i(TAG, "GooglePlayService error dialog is canceled.");
+                }
+            }
+        }).show();
     }
 
 
